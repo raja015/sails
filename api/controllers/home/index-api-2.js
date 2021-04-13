1c2d7b0d6884a12e6b4e5fa1f6c9feb958fce1d4
raja015 <40671459+raja015@users.noreply.github.com>
2021-04-14 00:58:48 +0530

    final

---
+++ api/controllers/home/index-api-2.js
@@ -0,0 +1,33 @@
+module.exports = {
+
+
+  friendlyName: 'Index api 2',
+
+
+  description: '',
+
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+  api1:function(req,res){
+    res.view('cool');
+  }
+
+  // fn: async function (req,res) {
+  //   console.log(req);
+
+  //   // res.send('cool');
+  //   // All done.
+
+
+  // }
+
+
+};
---
+++ api/controllers/home/index.js
@@ -0,0 +1,39 @@
+// module.exports ={
+
+
+//   testingApi1:function(req,res){
+//     console.log(req.param);
+//   //   var id = req.param('id');
+//   //   res.send(id);
+//     // User
+//     //   .findOne({
+//     //     empnum: id
+//     //   })
+//     //   .exec(function(err, data) {
+//     //     if (err) {
+//     //       return res.json({
+//     //         error: err
+//     //       });
+//     //     }
+//     //     if (!data) {
+//     //       return res.notFound();
+//     //     }
+//     //     else{
+//     //       return res.json({
+//     //         notFound: false,
+//     //         userData: user
+//     //       });
+//     //     }
+//     //   }
+//     // }
+
+//   }
+
+
+// };
+
+
+
+exports.testing = (req, res) => {
+  res.send('ccol');
+};
